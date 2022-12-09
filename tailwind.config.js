a3a1d7fb18a3db68a6d7d7943b17d0c31af309f1
Abhiram Reddy <nrabhiramr@gmail.com>
2022-12-09 14:02:15 +0530

    complete ui design for elements and pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ['./dist/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+      colors: {
+        dark: {
+          50: '#444444',
+          100: '#282424',
+          200: '#2A2A2A',
+          300: '#191919'
+        },
+        light: {
+          100: '#FFFFFF',
+          200: '#EEEEEE',
+          300: '#DDDDDD',
+          400: '#d5d5d5',
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
